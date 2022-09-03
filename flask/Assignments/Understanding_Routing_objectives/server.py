7520b84fd2d587bec84646f3f8c6b509bd5d7acd
Abby-Richardson <abbyrich27@gmail.com>
2022-09-03 16:27:01 -0400

    Playground

--- flask/Assignments/Understanding_Routing_objectives/server.py
+++ flask/Assignments/Understanding_Routing_objectives/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def hello_world():
     return 'Hello World!'  # Return the string 'Hello World!' as a response
 
-@app.route('/Dojo!')
+@app.route('/happy')
 def Dojo():
     return "Dojo!"
 
@@ -20,5 +20,10 @@ def repeat_word(word, number):
 
 
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
